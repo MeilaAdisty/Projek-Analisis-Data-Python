a28ac82ec64095330f5034611eccd92a33ba359b
xza_yay <160561377+MeilaAdisty@users.noreply.github.com>
2024-09-30 22:54:36 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Memuat data
-day_df = pd.read_csv('all_data (1).csv')
+day_df = pd.read_csv("dashboard/all_data (1).csv")
 
 # Judul aplikasi
 st.title("Analisis Penyewaan Sepeda Berdasarkan Cuaca dan Tahun 2012")
